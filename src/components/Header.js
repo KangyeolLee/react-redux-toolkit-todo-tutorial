386a5842b7b48dad87413f038fb2ffcf70d3a1e6
KG <acrkdduf@naver.com>
2021-10-19 23:15:57 +0900

    항목 입력 로직 및 항목 추가 dispatch 연결

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
+import { add as addTodo } from "../states/todos";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
 function Header() {
+  const [todo, setTodo] = useState("");
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (e) => {
+    if (!(e.keyCode === 13 || e.key === "Enter")) return;
+
+    const text = todo.trim();
+    if (!text) return;
+
+    setTodo("");
+    dispatch(addTodo(text));
+  };
+
   return (
     <header className="header">
       <h1>TODOs</h1>
       <input
+        value={todo}
+        onInput={(e) => setTodo(e.target.value)}
+        onKeyDown={handleAddTodo}
         className="todo-input"
         placeholder="할 일 목록을 입력하세요..."
         autoFocus
