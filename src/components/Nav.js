e3d9fb71569dd673bb910309d2f1c8ac1663bbbc
KG <acrkdduf@naver.com>
2021-10-19 23:17:12 +0900

    남은 항목 출력 및 타입에 따른 리스트 필터링과 완료항목 제거 dispatch 연결

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,56 @@
+import {
+  clearCompleted as clearCompletedTodo,
+  filter as filterTodo,
+} from "../states/todos";
+import { useDispatch, useSelector } from "react-redux";
+
+const filterTypeSelector = (state) => state.todos.filterType;
+const todoCountSelector = (state) =>
+  state.todos.items.filter((todo) => !todo.done).length;
+
 function Nav() {
+  const dispatch = useDispatch();
+  const filterType = useSelector(filterTypeSelector);
+  const todoCount = useSelector(todoCountSelector);
+
   return (
     <nav className="nav">
-      <span className="todo-count">남은 항목 : 0</span>
+      <span className="todo-count">남은 항목 : {todoCount}</span>
       <ul className="filter-options">
         <li>
-          <a className="selected" href="#/">
+          <a
+            className={filterType === "all" ? "selected" : ""}
+            onClick={() => dispatch(filterTodo("all"))}
+            href="#/"
+          >
             모두
           </a>
         </li>
         <li>
-          <a href="#/active">미완</a>
+          <a
+            className={filterType === "do" ? "selected" : ""}
+            onClick={() => dispatch(filterTodo("do"))}
+            href="#/active"
+          >
+            미완
+          </a>
         </li>
         <li>
-          <a href="#/completed">완료</a>
+          <a
+            className={filterType === "done" ? "selected" : ""}
+            onClick={() => dispatch(filterTodo("done"))}
+            href="#/completed"
+          >
+            완료
+          </a>
         </li>
       </ul>
-      <button className="clear-button">완료항목 제거</button>
+      <button
+        onClick={() => dispatch(clearCompletedTodo())}
+        className="clear-button"
+      >
+        완료항목 제거
+      </button>
     </nav>
   );
 }
