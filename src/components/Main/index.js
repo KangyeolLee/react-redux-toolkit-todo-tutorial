5dc99c09cee628996f9c43cd1931ff1b8b289fc5
KG <acrkdduf@naver.com>
2021-10-20 00:33:05 +0900

    일괄 체크 적용 dispatch 로직 적용

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,8 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import Todo from "./Todo";
 import { createSelector } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
+import { toggleAll } from "../../states/todos";
 
 const todosSelector = (state) => state.todos.items;
 const filterTypeSelector = (state) => state.todos.filterType;
@@ -19,15 +21,35 @@ const filteredTodosSelector = createSelector(
   }
 );
 
+const isAllCheckedSelector = (state) =>
+  state.todos.items.every((todo) => todo.done);
+
 function Main() {
+  const dispatch = useDispatch();
   const todos = useSelector(filteredTodosSelector);
   const Todos = todos.map((todo) => <Todo key={todo.id} {...todo} />);
 
+  const isAllChecked = useSelector(isAllCheckedSelector);
+
   return (
     <section className="main">
-      <input id="toggle-all" className="toggle-all" type="checkbox" />
-      <label htmlFor="toggle-all">모두 완료</label>
-      <ul className="todo-list">{Todos}</ul>
+      <div className="input-wrapper">
+        <input
+          checked={isAllChecked}
+          onChange={(e) => dispatch(toggleAll(e.target.checked))}
+          id="toggle-all"
+          className="toggle-all"
+          type="checkbox"
+        />
+        <label htmlFor="toggle-all">모두 체크</label>
+      </div>
+      <ul className="todo-list">
+        {Todos.length > 0 ? (
+          Todos
+        ) : (
+          <li className="warning-message">Oops! 해당 목록이 없습니다.</li>
+        )}
+      </ul>
     </section>
   );
 }
