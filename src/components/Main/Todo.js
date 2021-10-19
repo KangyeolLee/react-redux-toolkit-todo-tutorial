e9d379c822aad43a79064343141958a436d1d725
KG <acrkdduf@naver.com>
2021-10-20 00:33:15 +0900

    수정 및 제거 로직 dispatch 연동

--- src/components/Main/Todo.js
+++ src/components/Main/Todo.js
@@ -1,11 +1,55 @@
-import { check as checkTodo } from "../../states/todos";
+import {
+  check as checkTodo,
+  edit as editTodo,
+  remove as removeTodo,
+} from "../../states/todos";
+import { useEffect, useRef, useState } from "react";
+
 import { useDispatch } from "react-redux";
+
+function getClassName({ done, isEdit }) {
+  const classNames = [];
+  if (done) {
+    classNames.push("completed");
+  }
+
+  if (isEdit) {
+    classNames.push("editing");
+  }
+
+  return classNames.join(" ");
+}
 
 function Todo({ id, done, text }) {
   const dispatch = useDispatch();
+  const [value, setValue] = useState(text);
+  const [isEdit, setEdit] = useState(false);
+  const $input = useRef(null);
+
+  useEffect(() => {
+    if (isEdit) {
+      $input.current.focus();
+    }
+  }, [isEdit]);
+
+  const edit = () => {
+    const text = value.trim();
+    if (!text) return;
+
+    setEdit(false);
+    dispatch(editTodo({ id, text }));
+  };
+
+  const handleEnter = (e) => {
+    if (!(e.keyCode === 13 || e.key === "Enter")) return;
+
+    edit();
+  };
+
+  const className = getClassName({ done, isEdit });
 
   return (
-    <li className={done ? "completed" : ""}>
+    <li className={className}>
       <div className="view">
         <input
           className="toggle"
@@ -15,10 +59,19 @@ function Todo({ id, done, text }) {
             dispatch(checkTodo({ id, checked: e.target.checked }))
           }
         />
-        <label>{text}</label>
-        <button className="destroy" />
+        <label onDoubleClick={() => setEdit(true)}>{text}</label>
+        <input
+          className="edit"
+          onInput={(e) => setValue(e.target.value)}
+          onKeyDown={handleEnter}
+          onBlur={() => setEdit(false)}
+          ref={$input}
+          value={value}
+        />
+        <button className="destroy" onClick={() => dispatch(removeTodo(id))}>
+          제거
+        </button>
       </div>
-      <input className="edit" value="투두 타닷타닷타" />
     </li>
   );
 }
