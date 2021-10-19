ee8aaee2ba6da73a5ca959f4199173d147493771
KG <acrkdduf@naver.com>
2021-10-19 22:41:27 +0900

    App 컴포넌트 레이아웃 구성 및 스타일링 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./App.scss";
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+
 function App() {
-  return <div className="App"> hello wolrd </div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
